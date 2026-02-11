ba20296f07470d9a214a75c4a0cdfeea8f4ac7fe
Ogungbade Shalom <badeshalom@gmail.com>
2026-02-11 23:17:11 +0100

    changed demo account logins on login page

--- frontend/src/pages/auth/Login.jsx
+++ frontend/src/pages/auth/Login.jsx
@@ -166,9 +166,9 @@ const Login = () => {
                 <span className="font-semibold">Demo Accounts:</span>
               </p>
               <p className="text-xs text-gray-500 mt-1">
-                Donor: donor@test.com | Recipient: recipient@test.com | Admin: admin@test.com
+                Donor: donor@test.com | Recipient: recipient@test.com | Admin: admin@bloodbank.com 
               </p>
-              <p className="text-xs text-gray-500">Password: Test1234</p>
+              <p className="text-xs text-gray-500">Password: Test1234 AdminPassword: Admin123</p>
             </div>
           </div>
         </div>
